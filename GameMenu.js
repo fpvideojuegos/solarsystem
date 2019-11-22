5f71a401e952c1deda9a626bf8622bf729116f73
Miguel <miguel.solis.lb@gmail.com>
2019-11-22 08:59:34 +0100

    Uploaded final enemy Level 1 music

--- GameMenu.js
+++ GameMenu.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class GameMenu extends Phaser.Scene {
     constructor() {
       super("GameMenu");     
@@ -24,4 +25,32 @@ class GameMenu extends Phaser.Scene {
     }); 
       // this.input.on("pointerdown",() => this.scene.start("playGame"));
     }
+=======
+class GameMenu extends Phaser.Scene {
+    constructor() {
+      super("GameMenu");     
+    }
+
+    create(){        
+      //  const menuButton = this.add.bitmapText(config.width / 2 - 75, config.height / 2, "pixelFont", "START " , 40); 
+     //  this.add.bitmapText(config.width / 2 - 75, config.height / 2, "pixelFont", "START " , 40); 
+       /* menuButton.setInteractive().on('pointerdown', () => { 
+            this.scene.start("playGame");
+        }); */
+        this.button1 = this.add.bitmapText(10, 200, "Fuente", "1 Player" , 30); 
+        this.button2 = this.add.bitmapText(140, 200, "Fuente", "2 Player" , 30); 
+
+        this.button1.setInteractive().on('pointerdown', () => { 
+          this.scene.launch('HUD');
+          this.scene.start("playGame");
+        this.registry.set("players", 1);
+      }); 
+      this.button2.setInteractive().on('pointerdown', () => { 
+        this.scene.launch('HUD');
+        this.scene.start("playGame");
+      this.registry.set("players",2); // se pueden guardar objetos en el 2
+    }); 
+      // this.input.on("pointerdown",() => this.scene.start("playGame"));
+    }
+>>>>>>> Stashed changes
 }
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var gameSettings = {
     playerSpeed: 200,
     maxPowerups: 2,
@@ -20,4 +21,28 @@ var gameSettings = {
   
   
   var game = new Phaser.Game(config);
+=======
+var gameSettings = {
+    playerSpeed: 200,
+    maxPowerups: 2,
+    powerUpVel: 50,
+  }
+  
+  var config = {
+    width: 500,
+    height: 700,
+    backgroundColor: 0x000000 ,
+    scene: [Preload, Level1, GameMenu],
+    pixelArt: true,
+    physics: {
+      default: "arcade",
+      arcade:{
+          debug: false
+      }
+    }
+  }
+  
+  
+  var game = new Phaser.Game(config);
+>>>>>>> Stashed changes
   
