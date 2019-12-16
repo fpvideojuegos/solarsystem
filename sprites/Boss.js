257439555cbf7f65b5c4a913ffbf45ae037a0fd7
Antonio Herrera Fonseca <43568359+aherreracode@users.noreply.github.com>
2019-12-16 02:30:54 +0100

    + Fix

--- sprites/Boss.js
+++ sprites/Boss.js
@@ -1,5 +1,47 @@
 class Boss extends Enemy {
   constructor(config) {
 
+    super(config);
+
+    ///// PARAMS /////
+    this.key = config.key;
+    this.value = config.value;
+    this.scene = config.scene;
+    this.speed = config.speed;
+
+    //Physics
+    config.scene.physics.world.enable(this);
+    config.scene.add.existing(this);
+    this.setInteractive();
+
+    //Animation
+    this.play(config.anim);
+
+  }
+
+  moveBoss() {
+
+    this.y += this.speed;
+
+    if (this.y > config.height) {
+      this.scene.time.addEvent({
+        delay: 1000,
+        callback: () => {
+          this.resetEnemy();
+        },
+        callbackScope: this,
+        loop: false
+      });
     }
   }
+
+  resetEnemy() {
+
+    this.y = -50;
+
+    var randomX = Phaser.Math.Between(0, config.width);
+
+    this.x = randomX;
+  }
+
+}
