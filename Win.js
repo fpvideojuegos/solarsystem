c7b750ac92d8034286513ef4f7997737409bc28d
Antonio <aherrera.code@outlook.com>
2019-12-16 02:24:05 +0100

    + Fix

--- Win.js
+++ Win.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Win extends Phaser.Scene {
     constructor() {
       super("Win");
@@ -24,31 +23,4 @@ class Win extends Phaser.Scene {
       this.startButton = this.add.bitmapText(config.width / 2, config.height / 3, "pixelFont", "You win!", 48).setOrigin(0.5, 0.5);
   
     }
-=======
-class Win extends Phaser.Scene {
-    constructor() {
-      super("Win");
-    }
-  
-    create() {
-      
-      //Music of this scene
-      this.music = this.sound.add("MusMenu");
-      var musMenu = {
-        mute: false,
-        volume: 1,
-        rate: 1,
-        detune: 0,
-        seek: 0,
-        loop: false,
-        delay: 0
-      }
-      this.music.play(musMenu); 
-      
-      this.tittle = this.add.image(config.width / 2, config.height / 5, 'Tittle').setScale(2.5);
-  
-      this.startButton = this.add.bitmapText(config.width / 2, config.height / 3, "pixelFont", "You win!", 48).setOrigin(0.5, 0.5);
-  
-    }
->>>>>>> Antonio
   }
--- sprites/Zero.js
+++ sprites/Zero.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Zero extends Enemy {
     constructor(config) {
         super(config);
@@ -26,33 +25,4 @@ class Zero extends Enemy {
         var randomX = Phaser.Math.Between(0, config.width);
         this.x = randomX;
     }
-=======
-class Zero extends Enemy {
-    constructor(config) {
-        super(config);
-
-        //Animation
-        this.play("Enemy");
-    }
-
-    moveEnemy() {
-        this.y += this.speed;
-
-        if (this.y > config.height) {
-            this.scene.time.addEvent({
-                delay: 1000,
-                callback: () => {
-                    this.resetEnemy();
-                },
-                callbackScope: this,
-                loop: false
-            });
-        }
-    }
-    resetEnemy() {
-        this.y = -25;
-        var randomX = Phaser.Math.Between(0, config.width);
-        this.x = randomX;
-    }
->>>>>>> Antonio
 } 
